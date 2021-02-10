662b973f54f0d3b599b1192cbe69b885bf17f39c
Marcus Chok <marcus.chok110@gmail.com>
2021-02-10 13:59:01 -0500

    Refactor common REST routes using single function and add auth router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,27 @@
 import express from 'express';
-import userRouter from './userRouter';
+import ExpenseController from '../controllers/ExpenseController';
+import UserController from '../controllers/UserController';
+import RestRouter from './RestRouter';
+import authRouter from './authRouter';
 
+// Initialize routers for each resource
 const router = express.Router();
+const userRouter = express.Router();
+const expenseRouter = express.Router();
+
+// Create controllers for each router
+const userController = new UserController();
+const expenseController = new ExpenseController();
 
 // Map REST routes to routers
+RestRouter.mapRoutes(userRouter, userController);
+RestRouter.mapRoutes(expenseRouter, expenseController);
+
+// Configure custom behaviours
+userRouter.use('/:userId/expenses', expenseRouter);
+
+// Wrap all routes in one router
 router.use('/users', userRouter);
+router.use('/auth', authRouter);
 
 export default router;
