d92f43254d9ac639dde5c95826f6af903103f2c9
Marcus Chok <marcus.chok110@gmail.com>
2021-02-11 16:01:58 -0500

    Add documentation for exported type

--- backend/src/models/Model.ts
+++ backend/src/models/Model.ts
@@ -1,3 +1,12 @@
+/**
+ * SchemaFields type to keep mongoose model in sync with interface.
+ *
+ * Note: only field keys (not values) are enforced.
+ * @template IModel interface for your model to enforce certain fields
+ * @example
+ * interface IUser {}
+ * const UserSchemaFields: SchemaFields<IUser> = {}
+ */
 type SchemaFields<IModel> = Record<keyof IModel, any>;
 
 export default SchemaFields;
