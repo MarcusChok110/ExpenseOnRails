69da2059ad329f29e0dffdc25f9753ff0a9e9024
Marcus Chok <marcus.chok110@gmail.com>
2021-02-11 16:02:44 -0500

    Create account model to store individual user data

---
+++ backend/src/models/Account.ts
@@ -0,0 +1,54 @@
+/**
+ * Account model for user data.
+ * Contains fields such as transactions and expenses.
+ */
+import mongoose, { Schema, Document } from 'mongoose';
+import SchemaFields from './Model';
+import { TransactionDoc } from './Transaction';
+import { UserDoc } from './User';
+
+interface IAccount {
+  categories: string[];
+  balance: number;
+  budget: number;
+  expenses: number;
+  transactions: TransactionDoc['_id'][] | TransactionDoc[];
+  user: UserDoc['_id'] | UserDoc;
+}
+
+/**
+ * Default categories for Account category field
+ */
+const defaultCategories = [
+  'Clothing',
+  'Entertainment',
+  'Food',
+  'Health',
+  'Housing',
+  'Income',
+  'Insurance',
+  'Miscellaneous',
+  'Transportation',
+  'Utilities',
+];
+
+const AccountSchemaFields: SchemaFields<IAccount> = {
+  categories: { type: [String], required: true, default: defaultCategories },
+  balance: { type: Number, required: true, default: 0 },
+  budget: { type: Number, required: true, default: 0 },
+  expenses: { type: Number, required: true, default: 0 },
+  transactions: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Transaction',
+    required: true,
+  },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+};
+
+const AccountSchema = new Schema(AccountSchemaFields);
+
+export interface AccountDoc extends Document, IAccount {}
+
+const Account = mongoose.model<AccountDoc>('Account', AccountSchema);
+
+export default Account;
