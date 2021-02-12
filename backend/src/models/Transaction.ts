2bef43e60d7a3c21cae3aedb4847aa084e30d720
Marcus Chok <marcus.chok110@gmail.com>
2021-02-12 10:49:44 -0500

    Add export for base interface and add validation function

--- backend/src/models/Transaction.ts
+++ backend/src/models/Transaction.ts
@@ -6,22 +6,35 @@ import mongoose, { Schema, Document } from 'mongoose';
 import SchemaFields from './Model';
 import { AccountDoc } from './Account';
 
-interface ITransaction {
+export interface ITransaction {
   account: AccountDoc['_id'] | AccountDoc;
   amount: number;
   category: string;
   date: Date;
-  description: string;
+  description?: string;
   title: string;
   type: 'expense' | 'revenue';
 }
+
+/**
+ * Validates all fields except account (added later) and description (optional)
+ */
+export const validateFields = (transaction: ITransaction): boolean => {
+  return (
+    !!transaction.amount &&
+    !!transaction.category &&
+    !!transaction.date &&
+    !!transaction.title &&
+    !!transaction.type
+  );
+};
 
 const TransactionSchemaFields: SchemaFields<ITransaction> = {
   account: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
   amount: { type: Number, required: true },
   category: { type: String, required: true },
   date: { type: Date, required: true },
-  description: { type: String, required: true },
+  description: { type: String },
   title: { type: String, required: true },
   type: { type: String, required: true, enum: ['expense', 'revenue'] },
 };
