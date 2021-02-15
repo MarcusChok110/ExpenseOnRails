dba31fb48c5094f2086d81e7a84003dee8abbb95
Marcus Chok <marcus.chok110@gmail.com>
2021-02-14 22:16:44 -0500

    Rename to index and add functions for controller json responses

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,85 @@
+import { Request, Response, NextFunction } from 'express';
+import TransactionController from './TransactionController';
+import AccountController from './AccountController';
+import UserController from './UserController';
+
+/** Type of Express middleware functions */
+export type Middleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => void;
+
+/** Interface for REST Controllers */
+export default interface RestController {
+  /** GET /items to display a list of all items */
+  index: Middleware;
+
+  /** GET /items/new to show form to make new items */
+  new?: Middleware;
+
+  /** POST /items to add new item to database */
+  create: Middleware;
+
+  /** GET /items/:id to show info about one item */
+  show: Middleware;
+
+  /** GET /items/:id/edit to show edit form of one blog */
+  edit?: Middleware;
+
+  /** PUT /items/:id to update a particular item */
+  update: Middleware;
+
+  /** DELETE /items/:id to delete a particular item */
+  destroy: Middleware;
+}
+
+const accountController = new AccountController();
+const userController = new UserController();
+const transactionController = new TransactionController();
+
+export const controllers = {
+  accountController,
+  userController,
+  transactionController,
+};
+
+/**
+ * Returns an object with a success property set to false
+ * @param message message property for client
+ * @param options additional JSON to be returned in response
+ */
+export function createFailure(message: string, options?: object) {
+  if (!options) {
+    return {
+      success: false,
+      message,
+    };
+  }
+
+  return {
+    success: false,
+    message,
+    ...options,
+  };
+}
+
+/**
+ * Returns an object with a success property set to true
+ * @param options additional JSON to be returned in response
+ */
+export function createSuccess(options: object) {
+  return {
+    success: true,
+    ...options,
+  };
+}
+
+/**
+ * Common failure objects
+ */
+export const failures = {
+  BAD_USER: createFailure('Bad request: No User'),
+  NO_USER: createFailure('User not found'),
+  NO_ACCOUNT: createFailure('Account not found'),
+};
