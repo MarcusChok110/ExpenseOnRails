7e86c99a84bf80957d1a5bb04a60793cc2f908f2
Marcus Chok <marcus.chok110@gmail.com>
2021-02-15 11:55:01 -0500

    Remove unused NextFunction import

--- backend/src/controllers/AccountController.ts
+++ backend/src/controllers/AccountController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import Account from '../models/Account';
 import User from '../models/User';
 import RestController, {
