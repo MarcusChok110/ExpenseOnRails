39322a993047529e35744b158692f87ea576451c
Marcus Chok <marcus.chok110@gmail.com>
2021-02-15 13:01:46 -0500

    Refactor folder structure and route param name (id -> _id)

---
+++ backend/src/routes/api.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import RestController, { controllers } from '../controllers/index';
+
+// combined router for export
+const router = express.Router();
+
+// rest routers
+const userRouter = express.Router();
+const accountRouter = express.Router();
+const transactionRouter = express.Router();
+
+mapRoutes(userRouter, controllers.userController);
+mapRoutes(accountRouter, controllers.accountController);
+mapRoutes(transactionRouter, controllers.transactionController);
+
+router.use('/users', userRouter);
+
+router.use('./accounts', accountRouter);
+
+router.use('./transactions', transactionRouter);
+
+export default router;
+
+/**
+ * Maps the basic rest routes (index, create, show, update, destroy) to
+ * router using the functions provided by the controller object
+ *
+ * @param router Express Router to map routes to
+ * @param controller RestController instance containing methods
+ */
+function mapRoutes(router: express.Router, controller: RestController): void {
+  router.get('/', controller.index);
+  router.post('/', controller.create);
+  router.get('/:_id', controller.show);
+  router.put('/:_id', controller.update);
+  router.delete('/_id', controller.destroy);
+}
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,27 +1,13 @@
 import express from 'express';
-import ExpenseController from '../controllers/ExpenseController';
-import UserController from '../controllers/UserController';
-import RestRouter from './RestRouter';
-import authRouter from './authRouter';
+import authRouter from './auth';
+import apiRouter from './api';
+import passport from 'passport';
 
 // Initialize routers for each resource
 const router = express.Router();
-const userRouter = express.Router();
-const expenseRouter = express.Router();
-
-// Create controllers for each router
-const userController = new UserController();
-const expenseController = new ExpenseController();
-
-// Map REST routes to routers
-RestRouter.mapRoutes(userRouter, userController);
-RestRouter.mapRoutes(expenseRouter, expenseController);
-
-// Configure custom behaviours
-userRouter.use('/:userId/expenses', expenseRouter);
 
 // Wrap all routes in one router
-router.use('/users', userRouter);
 router.use('/auth', authRouter);
+router.use('/api', passport.authenticate('jwt', { session: false }), apiRouter);
 
 export default router;
