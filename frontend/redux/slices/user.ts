f38f5b0128f50cb78a62d55f356f615196fd4bd1
Marcus Chok <marcus.chok110@gmail.com>
2021-02-17 13:47:22 -0500

    Implement user slice functionality with fetching, updating, saving, etc

--- frontend/redux/slices/user.ts
+++ frontend/redux/slices/user.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User } from '../types';
 import { API_ROUTES } from '../../constants/index';
 import fetchOptions from '../../utils/fetchOptions';
@@ -7,9 +7,10 @@ export const userInitial: User = {
   loggedIn: false,
   email: '',
   account: '',
+  _id: '',
 };
 
-const fetchUserByJWT = createAsyncThunk(
+export const fetchUserByJWT = createAsyncThunk(
   'user/fetchByJWTStatus',
   async (jwt: string) => {
     const options = fetchOptions.createGet(jwt);
@@ -18,21 +19,56 @@ const fetchUserByJWT = createAsyncThunk(
   }
 );
 
+/**
+ * Thunk to save user email, first name, or last name changes
+ */
+export const saveUserChanges = createAsyncThunk(
+  'user/saveUserStatus',
+  async ({ jwt, _id, state }: { jwt: string; _id: string; state: User }) => {
+    const update = {
+      firstName: state.firstName,
+      lastName: state.lastName,
+      email: state.email,
+    };
+    const options = fetchOptions.createPut(jwt, { user: update });
+    const response = await fetch(`${API_ROUTES.USERS}/${_id}`, options);
+    return await response.json();
+  }
+);
+
 const userSlice = createSlice({
   name: 'user',
   initialState: userInitial,
-  reducers: {},
+  reducers: {
+    editFirstName: (state, { payload }: PayloadAction<string>) => {
+      state.firstName = payload;
+    },
+    editLastName: (state, { payload }: PayloadAction<string>) => {
+      state.lastName = payload;
+    },
+  },
   extraReducers: {
     [fetchUserByJWT.fulfilled.type]: (state, { payload }) => {
-      if (!payload.success) return;
+      if (!payload.success) {
+        console.log(payload);
+        throw new Error('Error fetching user');
+      }
       const { user } = payload;
 
       state.loggedIn = true;
       state.email = payload.user.email;
       state.account = payload.user.account;
+      state._id = payload.user._id;
 
       if (user.firstName) state.firstName = user.firstName;
       if (user.lastName) state.lastName = user.lastName;
+    },
+    [saveUserChanges.fulfilled.type]: (_state, { payload }) => {
+      if (!payload.success) {
+        console.log(payload);
+        throw new Error('Error saving user changes');
+      }
+      return payload.user;
     },
   },
 });
