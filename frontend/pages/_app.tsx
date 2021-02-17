1adc93dac809caeb9ff842ae9663795aaa5c8b25
Marcus Chok <marcus.chok110@gmail.com>
2021-02-16 23:59:02 -0500

    Add initial configuration for providing redux store to app

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/global.css';
 import { AppProps } from 'next/app';
+import store from '../redux/store';
+import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
