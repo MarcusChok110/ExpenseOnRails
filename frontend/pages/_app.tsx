499a32df91d1eb9d1c73f466fbed0ee2196197ad
Marcus Chok <marcus.chok110@gmail.com>
2021-02-18 23:26:56 -0500

    Add React import

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,7 @@ import store from '../redux/store';
 import { Provider } from 'react-redux';
 import Layout from '../components/Layout';
 import { CssBaseline } from '@material-ui/core';
+import React from "react";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
