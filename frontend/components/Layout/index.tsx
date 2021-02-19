0fcc899f63944d4dd723ba348b960bc1a73ef090
Marcus Chok <marcus.chok110@gmail.com>
2021-02-18 22:18:03 -0500

    Integrate Drawer (SideNav) into Layout; apply the necessary styles

--- frontend/components/Layout/index.tsx
+++ frontend/components/Layout/index.tsx
@@ -1,20 +1,42 @@
+import { makeStyles } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import 'fontsource-roboto';
 import Head from 'next/head';
 import React from 'react';
 import Header from './Header/index';
+import SideNav from './SideNav';
+
+export const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
 
 const Layout: React.FC = ({ children }) => {
+  const classes = useStyles();
+
   return (
-    <>
+    <div className={classes.root}>
       <Head>
         <title>Expense On Rails</title>
       </Head>
       <Header />
+      <SideNav />
       <Container>
-        <main>{children}</main>
+        <main className={classes.content}>
+          <div className={classes.toolbar}></div>
+          {children}
+        </main>
       </Container>
-    </>
+    </div>
   );
 };
 
