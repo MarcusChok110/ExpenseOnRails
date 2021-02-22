ab47a8eee551009149210f7082754efcfc2e40c3
Marcus Chok <marcus.chok110@gmail.com>
2021-02-22 10:29:58 -0500

    Refactor login and register page using FormOutline and SubmitButton components

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,42 +1,12 @@
-import {
-  Avatar,
-  Button,
-  Container,
-  Grid,
-  Link,
-  makeStyles,
-  Paper,
-  Typography,
-} from '@material-ui/core';
-import React from 'react';
-import useInput from '../components/Form/useInput';
-import NextLink from 'next/link';
+import { Grid, Link } from '@material-ui/core';
 import { LockOpen } from '@material-ui/icons';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: theme.spacing(4),
-  },
-  avatar: {
-    backgroundColor: theme.palette.primary.light,
-    margin: theme.spacing(2),
-  },
-  form: {
-    width: '100%',
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(2, 0, 2),
-  },
-}));
+import NextLink from 'next/link';
+import React from 'react';
+import FormOutline from '../components/Form/FormOutline';
+import SubmitButton from '../components/Form/SubmitButton';
+import useInput from '../components/Form/useInput';
 
 const Login: React.FC = () => {
-  const classes = useStyles();
-
   // Input Elements
   const [email, EmailInput] = useInput('Email Address', 'email');
   const [password, PasswordInput] = useInput('Password', 'password');
@@ -47,34 +17,18 @@ const Login: React.FC = () => {
   };
 
   return (
-    <Container maxWidth="sm">
-      <Paper className={classes.paper} elevation={3}>
-        <Avatar className={classes.avatar}>
-          <LockOpen />
-        </Avatar>
-        <Typography variant="h5">Sign In</Typography>
-        <form className={classes.form} onSubmit={handleSubmit} method="post">
-          {EmailInput}
-          {PasswordInput}
-          <Button
-            className={classes.submit}
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-          >
-            Sign In
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <NextLink href="/register" passHref>
-                <Link variant="body2">Don't have an account? Register</Link>
-              </NextLink>
-            </Grid>
-          </Grid>
-        </form>
-      </Paper>
-    </Container>
+    <FormOutline title="Log In" icon={<LockOpen />} handleSubmit={handleSubmit}>
+      {EmailInput}
+      {PasswordInput}
+      <SubmitButton>Sign In</SubmitButton>
+      <Grid container justify="flex-end">
+        <Grid item>
+          <NextLink href="/register" passHref>
+            <Link variant="body2">Don't have an account? Register</Link>
+          </NextLink>
+        </Grid>
+      </Grid>
+    </FormOutline>
   );
 };
 
