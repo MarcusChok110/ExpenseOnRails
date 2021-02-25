ae50b076b5206cd884b0c21740e4416065861b7b
Marcus Chok <marcus.chok110@gmail.com>
2021-02-24 21:25:49 -0500

    Add useEffects to fetch user data if jwt is found and on update

--- frontend/components/Layout/index.tsx
+++ frontend/components/Layout/index.tsx
@@ -2,7 +2,11 @@ import { makeStyles } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import 'fontsource-roboto';
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { accountActions } from '../../redux/slices/account';
+import { transactionsActions } from '../../redux/slices/transactions';
+import { selectUser, userActions } from '../../redux/slices/user';
 import Header from './Header/index';
 import SideNav from './SideNav';
 
@@ -22,6 +26,24 @@ const useStyles = makeStyles((theme) => ({
 
 const Layout: React.FC = ({ children }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  // fetch user on start
+  useEffect(() => {
+    const jwt = localStorage.getItem('jwt');
+    if (!jwt) return;
+    dispatch(userActions.fetchUserByJWT(jwt));
+  }, []);
+
+  // fetch account and transactions on user change
+  useEffect(() => {
+    const jwt = localStorage.getItem('jwt');
+    if (!user.loggedIn || !jwt) return;
+
+    dispatch(accountActions.fetchAccountById({ jwt, _id: user.account }));
+    dispatch(transactionsActions.fetchTransactions({ jwt }));
+  }, [user]);
 
   return (
     <div className={classes.root}>
