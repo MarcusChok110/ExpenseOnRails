2f0bbf2a7eb5c564202c27fafec8f2e8f4abe81f
Marcus Chok <marcus.chok110@gmail.com>
2021-02-24 21:24:35 -0500

    Create login form for user to log in and add logic

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,34 +1,90 @@
 import { Grid, Link } from '@material-ui/core';
 import { LockOpen } from '@material-ui/icons';
+import { useRouter } from 'next/dist/client/router';
 import NextLink from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import FormOutline from '../components/Form/FormOutline';
+import LoadingCircle from '../components/Form/LoadingCircle';
 import SubmitButton from '../components/Form/SubmitButton';
 import useInput from '../components/Form/useInput';
+import useLoading from '../components/useLoading';
+import useSnackbar from '../components/useSnackbar';
+import { userActions } from '../redux/slices/user';
+import { AUTH_ROUTES } from '../utils/constants';
+import fetchOptions from '../utils/fetchOptions';
 
 const Login: React.FC = () => {
-  // Input Elements
-  const [email, EmailInput] = useInput('Email Address', 'email');
-  const [password, PasswordInput] = useInput('Password', 'password');
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [disabled, setDisabled] = useState<boolean>(false); // to disable form components on login
 
-  // TODO: HANDLE SUBMIT TO LOG USER IN AND RETRIEVE INFO FROM DATABASE
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  // Input Elements
+  const [email, emailProps, EmailInput] = useInput('Email Address', 'email');
+  const [password, passwordProps, PasswordInput] = useInput(
+    'Password',
+    'password'
+  );
+
+  const [openWrongEmail, wrongEmailProps, WrongEmailSnackbar] = useSnackbar(
+    'Email not found'
+  );
+  const [
+    openWrongPassword,
+    wrongPasswordProps,
+    WrongPasswordSnackbar,
+  ] = useSnackbar('Incorrect password');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const user = { email, password };
+
+    try {
+      const response = await fetch(
+        AUTH_ROUTES.LOGIN,
+        fetchOptions.createAuthPost(user)
+      );
+      const json = await response.json();
+
+      if (json.success) {
+        setDisabled(true);
+
+        localStorage.setItem('jwt', json.token);
+        dispatch(userActions.fetchUserByJWT(json.token));
+        router.push('/');
+      } else {
+        if (json.message === 'Incorrect Email') openWrongEmail();
+        if (json.message === 'Incorrect Password') openWrongPassword();
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
+  const [loading, doSubmit] = useLoading(handleSubmit);
+
   return (
-    <FormOutline title="Log In" icon={<LockOpen />} handleSubmit={handleSubmit}>
-      {EmailInput}
-      {PasswordInput}
-      <SubmitButton>Sign In</SubmitButton>
-      <Grid container justify="flex-end">
-        <Grid item>
-          <NextLink href="/register" passHref>
-            <Link variant="body2">Don't have an account? Register</Link>
-          </NextLink>
+    <>
+      <FormOutline title="Log In" icon={<LockOpen />} handleSubmit={doSubmit}>
+        <EmailInput {...emailProps} required disabled={loading || disabled} />
+        <PasswordInput
+          {...passwordProps}
+          required
+          disabled={loading || disabled}
+        />
+        <SubmitButton disabled={loading || disabled}>Sign In</SubmitButton>
+        <Grid container justify="flex-end">
+          <Grid item>
+            <NextLink href="/register" passHref>
+              <Link variant="body2">Don't have an account? Register</Link>
+            </NextLink>
+          </Grid>
         </Grid>
-      </Grid>
-    </FormOutline>
+      </FormOutline>
+      <WrongEmailSnackbar {...wrongEmailProps} />
+      <WrongPasswordSnackbar {...wrongPasswordProps} />
+      <LoadingCircle display={loading || disabled} />
+    </>
   );
 };
 
