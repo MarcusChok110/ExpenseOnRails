22de2de3d560d770bca065049b6297dedfb03a34
Marcus Chok <marcus.chok110@gmail.com>
2021-02-27 15:12:26 -0500

    Create custom table head component for transaction keys

---
+++ frontend/components/ExpenseTable/ExpenseTableHead.tsx
@@ -0,0 +1,84 @@
+import {
+  Checkbox,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+} from '@material-ui/core';
+import React from 'react';
+import { Transaction } from '../../redux/types';
+import { Order } from './utils';
+
+interface HeadCell {
+  id: keyof Transaction;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: HeadCell[] = [
+  { id: 'title', label: 'Transaction', numeric: false },
+  { id: 'amount', label: 'Amount', numeric: true },
+  { id: 'description', label: 'Description', numeric: false },
+  { id: 'type', label: 'Type', numeric: false },
+  { id: 'category', label: 'Category', numeric: false },
+  { id: 'date', label: 'Date', numeric: false },
+];
+
+interface Props {
+  numSelected: number;
+  order: Order;
+  orderBy: string;
+  onSelectedAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof Transaction
+  ) => void;
+  rowCount: number;
+}
+
+const ExpenseTableHead: React.FC<Props> = (props) => {
+  const {
+    numSelected,
+    onRequestSort,
+    onSelectedAllClick,
+    order,
+    orderBy,
+    rowCount,
+  } = props;
+
+  const createSortHandler = (property: keyof Transaction) => (
+    event: React.MouseEvent<unknown>
+  ) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectedAllClick}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+export default ExpenseTableHead;
