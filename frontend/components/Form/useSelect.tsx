50c3ce184205b4070d0abfaf6c9dd9de979adaba
Marcus Chok <marcus.chok110@gmail.com>
2021-02-27 22:06:26 -0500

    Create custom useSelect hook for select form inputs

---
+++ frontend/components/Form/useSelect.tsx
@@ -0,0 +1,59 @@
+import {
+  FormControl,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+} from '@material-ui/core';
+import React, { useState } from 'react';
+
+export default function useSelect(
+  label: string,
+  id: string,
+  options: { value: string; label: string }[]
+): [string, Props, React.FC<Props>] {
+  const [value, setValue] = useState('');
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setValue(event.target.value as string);
+  };
+
+  const selectProps = { label, id, options, value, handleChange };
+  return [value, selectProps, FormSelect];
+}
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
+interface Props {
+  label: string;
+  id: string;
+  options: { value: string; label: string }[];
+  value: string;
+  handleChange: (event: React.ChangeEvent<{ value: unknown }>) => void;
+  required?: boolean;
+}
+
+const FormSelect: React.FC<Props> = ({
+  label,
+  id,
+  options,
+  value,
+  handleChange,
+  required,
+}) => {
+  const classes = useStyles();
+  return (
+    <FormControl className={classes.formControl} required={required}>
+      <InputLabel id={id}>{label}</InputLabel>
+      <Select labelId={id} value={value} onChange={handleChange}>
+        {options.map((option) => (
+          <MenuItem value={option.value}>{option.label}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
