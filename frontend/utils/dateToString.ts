c78530d99ba89921a48b8cfac1687456b4c857be
Marcus Chok <marcus.chok110@gmail.com>
2021-03-01 17:44:39 -0500

    Add function for parsing an isostring to the form input format

--- frontend/utils/dateToString.ts
+++ frontend/utils/dateToString.ts
@@ -4,3 +4,12 @@ export default function dateToString(dateString: string): string {
     date.getUTCMonth() + 1
   }/${date.getUTCDate()}/${date.getUTCFullYear()}`;
 }
+
+export function dateToFormString(dateString: string): string {
+  const date = new Date(dateString);
+  return `${date.getUTCFullYear().toString().padStart(4, '0')}-${(
+    date.getUTCMonth() + 1
+  )
+    .toString()
+    .padStart(2, '0')}-${date.getUTCDate().toString().padStart(2, '0')}`;
+}
